920cd4d6d35595a1f3727c5961a195bd6732a5ea
Thomas Proust <thomas.proust89@gmail.com>
2020-01-02 23:58:32 +0900

    [api] added extra logic to scan services for more dynamic results

--- api/services/scan.js
+++ api/services/scan.js
@@ -2,14 +2,66 @@
  * @description: This is a fake service with some logic to insert random data for the Results
  */
 
-const findings = require('./findings_sample.json');
+const STATUS = {
+    QUEUED: 'Queued',
+    IN_PROGRESS: 'In Progress',
+    SUCCESS: 'Success',
+    FAILURE: 'Failure',
+};
 
-const scan = repo => {
-    repo.status = 'pending';
-    repo.findings = findings;
-    repo.queuedAt = Date.now();
+const sample_finding = {
+    type: 'sast',
+    ruleId: 'G402',
+    location: {
+        path: 'connectors/apigateway.go',
+        positions: {
+            begin: {
+                line: 60,
+            },
+        },
+    },
+    metadata: {
+        description: 'TLS InsecureSkipVerify set true.',
+        severity: 'HIGH',
+    },
+};
 
-    return repo;
+const scan = repo => ({ ...repo, ...populateResultEntity() });
+
+const populateResultEntity = () => attributeDataFromStatus(chooseRandomStatus());
+
+const chooseRandomStatus = (statusList = [STATUS.QUEUED, STATUS.IN_PROGRESS, STATUS.SUCCESS, STATUS.FAILURE]) =>
+    statusList[Math.floor(Math.random() * statusList.length)];
+
+const attributeDataFromStatus = status => {
+    const data = { status, findings: [] };
+    if (status === STATUS.QUEUED) {
+        data.queuedAt = Date.now();
+    }
+
+    if (status === STATUS.IN_PROGRESS) {
+        data.scanningAt = Date.now();
+    }
+
+    if (status === STATUS.SUCCESS || status === STATUS.FAILURE) {
+        data.finishedAt = Date.now();
+        if (status === STATUS.SUCCESS) {
+            data.findings = attributeFindings();
+        }
+    }
+
+    return data;
+};
+
+const attributeFindings = (max = 5) => {
+    let nbOfFindings = Math.floor(Math.random() * max) + 1;
+    const findings = [];
+    while (nbOfFindings > 0) {
+        findings.push(sample_finding);
+        nbOfFindings--;
+    }
+
+    return findings;
 };
 
 module.exports = scan;
