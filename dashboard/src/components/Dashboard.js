79e2f3b911fd8468461df2bde5b457e22555ad5f
Thomas Proust <thomas.proust89@gmail.com>
2020-01-02 22:47:19 +0900

    [dashboard] fetch data from API

--- dashboard/src/components/Dashboard.js
+++ dashboard/src/components/Dashboard.js
@@ -2,16 +2,25 @@ import React, { useState, useEffect } from 'react';
 import ScanForm from './ScanForm';
 import ScanList from './ScanList';
 import { Container, Row, Col } from 'react-bootstrap';
+import axios from 'axios';
 
 const item = { repositoryName: 'name', status: 'pending', findings: [], queuedAt: Date.now() };
-const list = [item, item, item];
+
+const url = 'http://localhost:3090/results';
 
 const Dashboard = () => {
-    const [scans, setScans] = useState(list);
+    const [scans, setScans] = useState([]);
 
     const addScan = () => {
         setScans([...scans, item]);
     };
+
+    useEffect(() => {
+        (async () => {
+            const response = await axios.get(url);
+            setScans(response.data);
+        })();
+    }, []);
 
     return (
         <Container className="mt-5">
